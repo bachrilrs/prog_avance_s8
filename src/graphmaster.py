a7ceaf6e03ec387808c6b7647ebd08a088797778
bachrilrs <bachri.laroussi-labid@utoulouse.fr>
2026-02-22 19:35:49 +0100

    finished recoding in OOP the graph module

--- src/graphmaster.py
+++ src/graphmaster.py
@@ -4,6 +4,7 @@
 # M1 BBS - S7 Graphs
 """Graphmaster module: a simple graph class and related functions."""
 import math
+from collections import deque
 
 
 class Graph:
@@ -14,6 +15,16 @@ class Graph:
         self.directed = directed
         self.weighted = weighted
         self.weight_attribute = weight_attribute
+
+    def copy(self):
+        """Create a deep copy of the graph."""
+        g_copy = Graph(self.directed, self.weighted, self.weight_attribute)
+        for n, node in self.nodes.items():
+            g_copy.add_node(n, node.attributes.copy())
+        for u, neigh in self.edges.items():
+            for v, e in neigh.items():
+                g_copy.add_edge(u, v, e.attributes.copy())
+        return g_copy
 
     @classmethod
     def from_delim(cls, filename, column_separator='\t',
@@ -70,7 +81,8 @@ class Graph:
         return self.nodes[node_id]
 
     def add_edge(self, u_id, v_id, attributes=None):
-        """Add an edge from u_id to v_id with optional attributes. Creates nodes if they don't exist."""
+        """Add an edge from u_id to v_id with optional attributes.
+          Creates nodes if they don't exist."""
 
         u_node = self.add_node(u_id)
         v_node = self.add_node(v_id)
@@ -87,7 +99,7 @@ class Graph:
 
     def neighbors(self, node_id):
         """Return a sorted list of neighbors of a node."""
-        return sorted(self.edges.get(node_id, {}).keys())
+        return self.edges.get(node_id, {}).keys()
 
     def node_exists(self, n):
         """Check if a node exists in the graph."""
@@ -105,9 +117,7 @@ class Graph:
         return all_edges
 
     def nb_edges(self):
-        """Return the number of edges in the graph.
-        For undirected graphs, each edge is counted once.
-        """
+        """Return the number of edges in the graph"""
         total = sum(len(adj) for adj in self.edges.values())
         return total // (1 if self.directed else 2)
 
@@ -115,13 +125,12 @@ class Graph:
         """Check if an edge from u to v exists."""
         return self.node_exists(u) and v in self.edges[u]
 
-    def in_degree(self,node_id):
-        """Return the in-degree of a node (number of incoming edges).
-        Visits all nodes to count how many have an edge to node_id."""
-        cpt= 0
-        for other in self.nodes:
-            if node_id in self.neighbors(other):
-                cpt +=1
+    def in_degree(self, node_id):
+        """Return the in-degree of a node (number of incoming edges)."""
+        cpt = 0
+        for _, adj in self.edges.items():
+            if node_id in adj:
+                cpt += 1
         return cpt
 
     def out_degree(self,node_id):
@@ -183,6 +192,219 @@ class Graph:
             res[el] = len(self.filter_edges('relationship',el))
         return res
 
+    def bfs(self,s):
+        """Perform a breadth-first search (BFS) from source node s.
+
+        Parameters
+        ----------
+        g : dict
+            Graph object.
+        s : hashable
+            Source node identifier.
+
+        Returns
+        -------
+        tuple
+            (state, distance, predecessor) dictionaries:
+            - state[u] in {'Unexplored', 'Discovered', 'Finished'}
+            - distance[u] is the BFS distance from s (or -inf if unreachable)
+            - predecessor[u] is the parent of u in the BFS tree (or None).
+        """
+        state = {}
+        distance = {}
+        predecessor = {}
+
+        for node in self.nodes:
+            state[node] = 'Unexplored'
+            distance[node] = -math.inf # float('-inf') works too
+            predecessor[node] = None
+
+        state[s]= 'Discovered'
+        distance[s] = 0
+        queue = []
+        q = deque([s])
+        while queue :
+            u = q.popleft()
+            for v in self.neighbors(u):
+                if state[v]=='Unexplored':
+                    state[v] = 'Discovered'
+                    distance[v]= distance[u] + 1
+                    predecessor[v] = u
+                    queue.append(v)
+
+            state[u]='Finished'
+
+        return state , distance , predecessor
+
+    def path(self,bfs_res, source, target):
+        """Reconstruct the shortest path from source to target using BFS results.
+
+        Parameters
+        ----------
+        bfs_res : tuple
+            Result of bfs(g, source): (state, distance, predecessor).
+        source : hashable
+            Source node identifier.
+        target : hashable
+            Target node identifier.
+
+        Returns
+        -------
+        str or None
+            A string describing the path and its distance, or None if no path exists.
+        """
+        _, distance, predecessor = bfs_res
+
+        if source == target:
+            return [source]
+        route = []
+        cur = target
+        while cur is not None and cur != source:
+            route.append(cur)
+            cur = predecessor[cur]
+
+        if cur is None:
+            return None
+
+        route.append(source)
+        route.reverse()
+        return f"{route} \n La distance entre {source} et {target} est de {distance[target]}."
+
+    def dfs_visit(self,u,parcours):
+        """Recursive DFS visit from node u, updating the traversal structure.
+
+        Parameters
+        ----------
+        g : dict
+            Graph object.
+        u : hashable
+            Starting node for this DFS visit.
+        parcours : dict
+            DFS traversal structure containing state, times and edge classifications.
+        """
+        parcours['state'][u] = 'Discovered'
+        parcours['time'] += 1
+        parcours['discovered'][u]= parcours['time']
+
+        for v in self.neighbors(u):
+            if parcours['state'][v] == 'Unexplored':
+                parcours['predecessor'][v] = u
+                parcours['classification'][(u,v)] = 'TREE EDGE'
+                self.dfs_visit(v,parcours)
+            elif parcours['state'][v] == 'Discovered':
+                parcours['classification'][(u,v)] = 'BACK EDGE'
+            elif parcours['discovered'][u] > parcours['discovered'][v]:
+                parcours['classification'][(u,v) ] = "CROSS EDGE"
+            else:
+                parcours['classification'][(u,v)] = "FORWARD EDGE"
+        parcours['state'][u] = 'Finished'
+        parcours['time'] += 1
+        parcours['finished'][u] = parcours['time']
+
+    def dfs(self):
+        """Perform a depth-first search (DFS) on the whole graph.
+
+        Returns
+        -------
+        dict
+            Traversal information with keys:
+            'time', 'state', 'discovered', 'finished',
+            'classification', 'predecessor'.
+        """
+        parcours = {'time' : 0,
+            'state' : {},
+            'discovered' : {},
+            'finished' : {},
+            'classification' : {},
+            'predecessor' : {},
+        }
+
+        for u in self.nodes:
+            parcours['state'][u] = 'Unexplored'
+            parcours['predecessor'][u] = None
+
+
+        for v in self.nodes:
+            if parcours['state'][v] == 'Unexplored':
+                self.dfs_visit(v,parcours)
+        return parcours
+
+    def is_acyclic(self):
+        """Check whether the graph is acyclic using DFS edge classification.
+
+        Returns
+        -------
+        str
+            A textual message indicating if the graph is a DAG or contains a cycle.
+        """
+
+        parcours = self.dfs()
+        for el in parcours['classification'].values():
+            if el == 'BACK EDGE':
+                return False
+        return True
+
+    def topological_sort(self):
+        """Compute a topological ordering of the nodes of a DAG.
+
+        The graph must be acyclic; otherwise a ValueError is raised.
+
+        Returns
+        -------
+        list of hashable
+            List of node identifiers ordered topologically.
+        """
+        # Check graph is acyclic.
+        if not self.is_acyclic():
+            raise ValueError("impossible to sort: DAG.")
+        if self.nb_nodes() == 0:
+            return False
+
+        parcours = self.dfs()
+
+        fins = parcours['finished']
+
+
+        sorted_nodes = list(fins.keys())
+        sorted_nodes.sort(key=lambda n: fins[n], reverse=True)
+
+
+        return sorted_nodes
+
+    def transpose(self):
+        """Return the transpose of the graph (reverse all edges)."""
+        if not self.directed:
+            return self.copy()
+
+        gt = Graph(directed=True, weighted=self.weighted, weight_attribute=self.weight_attribute)
+
+        # copy nodes
+        for n, node in self.nodes.items():
+            gt.add_node(n, node.attributes.copy())
+
+        # reverse edges
+        for u, neigh in self.edges.items():
+            for v, e in neigh.items():
+                gt.add_edge(v, u, e.attributes.copy())
+
+        return gt
+
+    def induced_subgraph(self, induced_nodes):
+        """Return the subgraph induced by a given set of nodes."""
+        sg = Graph(self.directed, self.weighted, weight_attribute=self.weight_attribute)
+
+        node_set = set(induced_nodes)
+
+        for n in node_set:
+            if n in self.nodes:
+                sg.add_node(n, self.nodes[n].attributes.copy())
+
+        for u in node_set:
+            for v in self.neighbors(u):
+                if v in node_set:
+                    sg.add_edge(u, v, self.edges[u][v].attributes.copy())
+
+        return sg
 
 class Node:
     """Node class representing a graph node with optional attributes."""
@@ -210,283 +432,6 @@ class Edge:
 
     def __str__(self):
         return f"Edge({self.source.id} -> {self.target.id})"
-
-
-
-
-# to do
-
-def enqueue(queue,element):
-    """Append an element at the end of a queue (FIFO list)."""
-    queue.append(element)
-
-
-def dequeue(queue):
-    """Remove and return the first element of a queue (FIFO list).
-
-    Raises
-    ------
-    IndexError
-        If the queue is empty.
-    """
-    if len(queue)==0:
-        raise IndexError("Empty queue")
-    return queue.pop(0) # first in first out, so pop(0)
-
-
-def relationships(g):
-    """Print the number of edges for each relationship type in the graph."""
-    liste = ['is a' , 'part of' , 'annotation']
-    res = {}
-    for el in liste:
-        res[el] = len(filter_edges(g,'relationship',el))
-    return res
-
-
-def bfs(g,s):
-    """Perform a breadth-first search (BFS) from source node s.
-
-    Parameters
-    ----------
-    g : dict
-        Graph object.
-    s : hashable
-        Source node identifier.
-
-    Returns
-    -------
-    tuple
-        (state, distance, predecessor) dictionaries:
-        - state[u] in {'Unexplored', 'Discovered', 'Finished'}
-        - distance[u] is the BFS distance from s (or -inf if unreachable)
-        - predecessor[u] is the parent of u in the BFS tree (or None).
-    """
-    state = {}
-    distance = {}
-    predecessor = {}
-
-    for node in nodes(g):
-        state[node] = 'Unexplored'
-        distance[node] = -math.inf # float('-inf') works too
-        predecessor[node] = None
-
-    state[s]= 'Discovered'
-    distance[s] = 0
-    queue = []
-    enqueue(queue,s)
-    while queue :
-        u = dequeue(queue)
-        for v in neighbors(g,u):
-            if state[v]=='Unexplored':
-                state[v] = 'Discovered'
-                distance[v]= distance[u] + 1
-                predecessor[v] = u
-                enqueue(queue,v)
-
-        state[u]='Finished'
-
-    return state , distance , predecessor
-
-
-def path(bfs_res, source, target):
-    """Reconstruct the shortest path from source to target using BFS results.
-
-    Parameters
-    ----------
-    bfs_res : tuple
-        Result of bfs(g, source): (state, distance, predecessor).
-    source : hashable
-        Source node identifier.
-    target : hashable
-        Target node identifier.
-
-    Returns
-    -------
-    str or None
-        A string describing the path and its distance, or None if no path exists.
-    """
-    _, distance, predecessor = bfs_res
-
-    if source == target:
-        return [source]
-    route = []
-    cur = target
-    while cur is not None and cur != source:
-        route.append(cur)
-        cur = predecessor[cur]
-
-    if cur is None:
-        return None
-
-    route.append(source)
-    route.reverse()
-    return f"{route} \n La distance entre {source} et {target} est de {distance[target]}."
-
-def dfs_visit(g,u,parcours):
-    """Recursive DFS visit from node u, updating the traversal structure.
-
-    Parameters
-    ----------
-    g : dict
-        Graph object.
-    u : hashable
-        Starting node for this DFS visit.
-    parcours : dict
-        DFS traversal structure containing state, times and edge classifications.
-    """
-    parcours['state'][u] = 'Discovered'
-    parcours['time'] += 1
-    parcours['discovered'][u]= parcours['time']
-
-    for v in neighbors(g,u):
-        if parcours['state'][v] == 'Unexplored':
-            parcours['predecessor'][v] = u
-            parcours['classification'][(u,v)] = 'TREE EDGE'
-            dfs_visit(g,v,parcours)
-        elif parcours['state'][v] == 'Discovered':
-            parcours['classification'][(u,v)] = 'BACK EDGE'
-        elif parcours['discovered'][u] > parcours['discovered'][v]:
-            parcours['classification'][(u,v) ] = "CROSS EDGE"
-        else:
-            parcours['classification'][(u,v)] = "FORWARD EDGE"
-    parcours['state'][u] = 'Finished'
-    parcours['time'] += 1
-    parcours['finished'][u] = parcours['time']
-
-def dfs(g):
-    """Perform a depth-first search (DFS) on the whole graph.
-
-    Returns
-    -------
-    dict
-        Traversal information with keys:
-        'time', 'state', 'discovered', 'finished',
-        'classification', 'predecessor'.
-    """
-    parcours = {'time' : 0,
-        'state' : {},
-        'discovered' : {},
-        'finished' : {},
-        'classification' : {},
-        'predecessor' : {},
-    }
-
-    for u in nodes(g):
-        parcours['state'][u] = 'Unexplored'
-        parcours['predecessor'][u] = None
-
-
-    for v in nodes(g):
-        if parcours['state'][v] == 'Unexplored':
-            dfs_visit(g,v,parcours)
-    return parcours
-
-
-
-def is_acyclic(g):
-    """Check whether the graph is acyclic using DFS edge classification.
-
-    Returns
-    -------
-    str
-        A textual message indicating if the graph is a DAG or contains a cycle.
-    """
-
-    parcours = dfs(g)
-    for el in parcours['classification'].values():
-        if el == 'BACK EDGE':
-            return False
-    return True
-
-
-
-def topological_sort(g):
-    """Compute a topological ordering of the nodes of a DAG.
-
-    The graph must be acyclic; otherwise a ValueError is raised.
-
-    Returns
-    -------
-    list of hashable
-        List of node identifiers ordered topologically.
-    """
-    # Check graph is acyclic.
-    if not is_acyclic(g):
-        raise ValueError("impossible to sort: DAG.")
-    if nb_nodes(g) == 0:
-        return False
-
-    parcours = dfs(g)
-
-    fins = parcours['finished']
-
-
-    sorted_nodes = list(fins.keys())
-    sorted_nodes.sort(key=lambda n: fins[n], reverse=True)
-
-
-    return sorted_nodes
-
-def transpose_graph(g):
-    """Return the transpose of a directed graph.
-
-    All nodes are copied, and for each edge u→v an edge v→u is created.
-
-    Parameters
-    ----------
-    g : dict
-        Original graph.
-
-    Returns
-    -------
-    dict
-        New graph representing the transpose of g.
-    """
-    gt = Graph(
-        directed = g['directed'],
-        weighted = g['weighted'],
-        weight_attribute = g['weight_attribute']
-    )
-
-    # Add nodes
-    for n in g['nodes']:
-        add_node(gt, n, g['nodes'][n].copy())
-
-    # Reverse edges
-    for u in g['edges']:
-        for v in g['edges'][u]:
-            add_edge(gt, v, u, g['edges'][u][v].copy())
-
-    return gt
-
-def induced_subgraph(g, induced_nodes):
-    """Build the subgraph induced by a given set of nodes.
-
-    Parameters
-    ----------
-    g : dict
-        Original graph.
-    nodes : iterable
-        Iterable of node identifiers to keep.
-
-    Returns
-    -------
-    dict
-        New graph containing only these nodes and edges between them.
-    """
-    sg = create_graph(g['directed'], g['weighted'], weight_attribute=g['weight_attribute'])
-
-    # add nodes (with their attributs)
-    for t in induced_nodes:
-        add_node(sg, t, g['nodes'][t].copy())
-
-    # add edges if they're in nodes
-    node_set = set(induced_nodes)
-    for t in induced_nodes:
-        for p in Graph.neighbors(g, t):
-            if p in node_set:
-                add_edge(sg, t, p, g['edges'][t][p].copy())
-    return sg
 
 
 if __name__ == "__main__":
